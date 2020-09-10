48d417de2c63ccb4b8af109bd0adfa1ff048eda9
saurabhsharma21 <saurabhsharma21@gmail.com>
2020-09-10 14:48:49 -0400

    Add files via upload
    Task 3 - Work in progress

---
+++ Task3.py
@@ -0,0 +1,38 @@
+# Create a list of the 10 elements of four different types of Data Type like int, 
+# string, complex and float.
+#1
+#firstlist = [1, 2.5, 3, 4.8, 7, "Saurabh", "3j+i", "nine", 10 ]
+
+#2
+# listtwo = [1,2,3,4,5]
+# slice_list= listtwo[2:3]
+# print(slice_list)
+
+#3
+# list3 = [1,2,3,4]
+# sum_list = 0
+# prod_list = 1
+
+# for i in list3:
+#     sum_list = sum_list + i
+#     prod_list = prod_list * i
+# print(sum_list)
+# print(prod_list)
+
+#4
+# minmax_list = [1,2,3,4,5,6,7,8,9,10]
+# print(min(minmax_list))
+# print(max(minmax_list))
+
+#5
+# list5 = [1,2,3,4,5,6,7,8,9]
+# newlist = []
+# for i in list5:
+#     if i%2 == 0:
+#         continue
+#     else:
+#         newlist.append(i)
+# print(newlist)
+
+#6
+sixlist = 
